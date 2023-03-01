f95c7df4d6c24ce4a91e5665fb899f34d2b6fd69
ly <137544897@qq.com>
2023-03-01 10:53:34 +0800

    feat:替换页面

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,92 +1,26 @@
 <template>
-  <el-container>
-    <el-aside width="200px">
-      <el-menu>
-        <el-submenu index="1">
-          <template slot="title"><i class="el-icon-message"></i>营销</template>
-          <el-menu-item-group>
-            <el-menu-item index="1-1" @click="goto('/sales')"
-              >商品模块</el-menu-item
-            >
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
-        </el-submenu>
-        <el-submenu index="2">
-          <template slot="title"><i class="el-icon-menu"></i>sales</template>
-          <el-menu-item index="2-1">sales模块</el-menu-item>
-        </el-submenu>
-        <el-submenu index="3">
-          <template slot="title"><i class="el-icon-setting"></i>links</template>
-          <el-menu-item index="3-1" @click="goto('/link')"
-            >我的收藏夹</el-menu-item
-          >
-          <el-menu-item index="3-2" @click="goto('/linkContainer')"
-            >我的收藏夹模板测试</el-menu-item
-          >
-        </el-submenu>
-      </el-menu>
-    </el-aside>
-
-    <el-container>
-      <el-header style="text-align: right; font-size: 12px">
-        <el-avatar class="title_avatar" :src="nikiLogo" size="small"
-          >汤圆</el-avatar
-        >
-        <span class="title_name">汤圆</span>
-      </el-header>
-
-      <el-main>
-        <router-view></router-view>
-      </el-main>
-    </el-container>
-  </el-container>
+  <div id="app">
+    <router-view/>
+  </div>
 </template>
-<style>
-.el-header {
-  background-color: #b3c0d1;
-  color: #333;
-  line-height: 60px;
-}
-
-.el-aside {
-  color: #333;
-}
-
-.title_avatar {
-  border: none;
-  vertical-align: middle;
-  margin-left: 10px;
-  margin-right: 6px;
-}
-
-.title_name {
-  font-family: "Roboto", "Lucida Grande", Verdana, Arial, sans-serif;
-  font-size: 14px;
-  color: #3c3c3c;
-}
-</style>
 
 <script>
-import nikiLogo from "../src/assets/niki_logo.png";
+import {onMounted} from "vue";
 
 export default {
-  data() {
-    return {
-      nikiLogo: nikiLogo,
-    };
-  },
-  onLaunch: function (option) {
-    console.log("sdfsfsdfsdfsdfsdf" + option.path);
-  },
-  onLoad: function (option) {
-    console.log("sdfsfsdfsdfsdfsdf" + option.path);
-  },
-  methods: {
-    goto(path) {
-      if (this.$router.currentRoute.path !== path) {
-        this.$router.replace(path);
-      }
-    },
-  },
-};
+  name: 'App',
+  mounted() {
+  }
+}
+
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,40 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import LinkCollection from '@/components/LinkCollection'
-// import Index from "@/components/index/index.vue";
 
-import sales from '../components/sales/sales.vue';
-import linkiee from '../components/linkiee/linkiee.vue';
-import link from '../components/link/link.vue';
-import linkContainer from '../components/link/link_container.vue';
 Vue.use(Router)
 
+const home = () => import("../pages/home/Home.vue")
+
+
+const routers = [
+  {
+    path: "/",
+    component: home
+  }
+]
+
 export default new Router({
-  routes: [
-    {
-      path: '/LinkCollection',
-      name: 'LinkCollection',
-      component: LinkCollection
-    },
-    {
-      path: '/sales',
-      name: 'sales',
-      component: sales
-    },
-    {
-      path: '/linkiee',
-      name: 'linkiee',
-      component: linkiee
-    },
-    {
-      path: '/link',
-      name: 'link',
-      component: link
-    },
-    {
-      path: '/linkContainer',
-      name: "linkContaienr",
-      component: linkContainer
-    }
-  ]
+  routes: routers,
+  strict: process.env.NODE_ENV !== 'production',
 })
