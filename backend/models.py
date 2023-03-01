ae1c217a7e3e1df03e8b5a629e43b50b2d509de1
ly <137544897@qq.com>
2023-03-01 18:32:25 +0800

    feat:表结构

--- backend/models.py
+++ backend/models.py
@@ -5,12 +5,11 @@ from django.db import models
 
 # Create your models here.
 class Link(models.Model):
-    linkId = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     linkDescription = models.CharField(max_length=200, verbose_name="链接描述")
     linkUrl = models.CharField(max_length=200, verbose_name="链接地址")
-    linkRemark = models.CharField(max_length=200, verbose_name='链接备注')
     userName = models.CharField(max_length=50, verbose_name='账户', default='niki')
     passWord = models.CharField(max_length=20, verbose_name='密码', default='123456')
+    env = models.IntegerField(verbose_name='对应环境0:测试，1集成，2生产', default=0)
 
     def __unicode__(self): return self.linkUrl
 
